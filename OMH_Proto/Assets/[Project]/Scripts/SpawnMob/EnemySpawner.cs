bbd310b8d4840ffbd8c45867615dc03446fc48a8
Dragomal <115795428+Dragomal@users.noreply.github.com>
2024-11-14 15:40:00 +0100

    add nest destroyed pre work on EnemySpawner

--- OMH_Proto/Assets/[Project]/Scripts/SpawnMob/EnemySpawner.cs
+++ OMH_Proto/Assets/[Project]/Scripts/SpawnMob/EnemySpawner.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _enemyPrefab;
+
+    private float _numberEnemies;
+    private bool _isNestDestroyed;
     
     public void SpawnMob(int numberOfMobs, int durationOfSpawn, MobTarget gasTankTarget)
     {
+        _numberEnemies = numberOfMobs;
+        _numberEnemies = Mathf.RoundToInt(_numberEnemies / 2);
+        
         GameObject tempObject = Instantiate(_enemyPrefab, transform.position, Quaternion.identity, transform);
 
         TargetFinder targetFinder = tempObject.GetComponentInChildren<TargetFinder>();
         targetFinder.Initialize(gasTankTarget);
         
         print("Spawn " + numberOfMobs + " enemies during " + durationOfSpawn);
+    }
+
+    public void DestroyNest()
+    {
+        _isNestDestroyed = true;
     }
 }
