2ed0372633aba8330dc93385c0672357ea319331
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2024-11-15 15:07:21 +0100

    mob target finder less buged

--- OMH_Proto/Assets/[Project]/Scripts/GameLoop/Mob/MobAttack.cs
+++ OMH_Proto/Assets/[Project]/Scripts/GameLoop/Mob/MobAttack.cs
@@ -5,18 +5,16 @@ using UnityEngine.Events;
 public class MobAttack : MonoBehaviour
 {
     [SerializeField] private TargetFinder _targetFinder;
-    [SerializeField] private Transform _attackTransform;
     [SerializeField] private AnimatorParametreSetter _animatorSetter;
     [Space]
     [SerializeField] private FloatReference _distanceTrigger;
-    [SerializeField] private FloatReference _damage;
     [SerializeField] private FloatReference _attackPerSecond;
     private float _attackTime;
 
     private void Update()
     {
         if (!_targetFinder.Target) return;
-        if (_targetFinder.TargetDistance < _distanceTrigger.Value)
+        if (_targetFinder.TargetDistance <= _distanceTrigger.Value)
         {
             _attackTime += Time.deltaTime;
             if (_attackTime > 1 / _attackPerSecond.Value)
