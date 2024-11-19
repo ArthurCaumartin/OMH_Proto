2a0cbba9a17016f412b9a161bf5e99cb209c8418
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2024-11-19 13:53:56 +0100

    Update RadialMenu.cs

--- OMH_Proto/Assets/[Project]/Scripts/UI/RadialMenu.cs
+++ OMH_Proto/Assets/[Project]/Scripts/UI/RadialMenu.cs
@@ -1,13 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class RadialMenu : MonoBehaviour
 {
     [SerializeField] private Button _buttonPrefab;
-    [SerializeField] private int _menuLenth = 3;
-    public Color _red = Color.red;
-    public Color _blue = Color.blue;
+    [SerializeField] private int _menuLenght = 3;
+    [SerializeField] private Color _red = Color.red;
+    [SerializeField] private Color _blue = Color.blue;
     [SerializeField] private float _size = 5;
     [SerializeField] private List<Button> _buttonList = new List<Button>();
 
@@ -16,32 +17,30 @@ public class RadialMenu : MonoBehaviour
         BakeMenu();
     }
 
-    public void Select()
-    {
-
-    }
-
     private void BakeMenu()
     {
-        if(_buttonList.Count > 0)
+        if (_buttonList.Count > 0)
         {
             foreach (var item in _buttonList)
             {
-                if(!item) continue;
+                if (!item) continue;
                 Destroy(item.gameObject);
             }
             _buttonList.Clear();
         }
 
-        for (int i = 0; i < _menuLenth; i++)
+        for (int i = 0; i < _menuLenght; i++)
         {
-            float time = Mathf.InverseLerp(0, _menuLenth - 1, i);
+            float time = Mathf.InverseLerp(0, _menuLenght, i);
             print("Time : " + time);
 
-            float radialTime = Mathf.Lerp(-360, 360, time);
+            float radialTime = Mathf.Lerp(0, 360, time);
+            radialTime *= Mathf.Deg2Rad;
             print("Raial Time : " + radialTime);
 
-            Vector3 newPos = new Vector3(Mathf.Cos(radialTime), Mathf.Sin(radialTime), 0);
+            Vector3 newPos = new Vector3(-Mathf.Cos(radialTime), Mathf.Sin(radialTime), 0);
+            print("New Pos for i = " + i + " => " + newPos);
+
             Button b = Instantiate(_buttonPrefab, transform);
             b.transform.localPosition = newPos * _size;
             b.GetComponent<Image>().color = Color.Lerp(_red, _blue, time);
@@ -49,8 +48,13 @@ public class RadialMenu : MonoBehaviour
         }
     }
 
+    public void Open(bool value)
+    {
 
+    }
 
-
-
+    private void OnOpenRadialMenu(InputValue value)
+    {
+        Open(value.Get<float>() > .5f);
+    }
 }
