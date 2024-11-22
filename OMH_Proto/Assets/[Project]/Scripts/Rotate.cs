2d996771ceb648e010207b4d004c79064d0a63b7
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2024-11-22 12:41:51 +0100

    Update Rotate.cs

--- OMH_Proto/Assets/[Project]/Scripts/Rotate.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Rotate.cs
@@ -6,9 +6,12 @@ public class Rotate : MonoBehaviour
 {
     [SerializeField] private Vector3 _axis = Vector3.up;
     [SerializeField] private float _speed = 5;
+    [SerializeField] private int _direction;
+
+    void Start() { _direction = Random.value > .5f ? -1 : 1; }
 
     void Update()
     {
-        transform.Rotate(_axis * _speed * Time.deltaTime);
+        transform.Rotate(_axis * _speed * Time.deltaTime * _direction);
     }
 }
