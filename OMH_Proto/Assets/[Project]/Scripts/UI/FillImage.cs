1fe84d407a070e4d4d8879da37a92e1db47cdf56
Dragomal <115795428+Dragomal@users.noreply.github.com>
2024-11-22 13:45:12 +0100

    Fix Jauge animation

---
+++ OMH_Proto/Assets/[Project]/Scripts/UI/FillImage.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class FillImage : MonoBehaviour
+{
+    [SerializeField] private Image _image;
+    [SerializeField] private FloatReference _fillDuration;
+    [SerializeField] private AnimationCurve _animationCurve;
+
+    private float tempFloat = 0;
+
+    public void StartFillImage()
+    {
+        DOTween.To(()=> tempFloat, x=> tempFloat = x, 1, _fillDuration.Value).SetEase(_animationCurve);
+        
+    //     float tempFloat = (float) ((double) _defenseDuration.Value / 10);
+    //     float tempFloat2 = (float) ((double) 1 / tempFloat);
+    //     _animator.speed = tempFloat2;
+    }
+
+    public void Update()
+    {
+        _image.fillAmount = tempFloat;
+    }
+}
