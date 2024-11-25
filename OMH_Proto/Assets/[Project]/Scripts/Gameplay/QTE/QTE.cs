a6ba2e01fca32e19be0d6ccb213c3875e110a0d9
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2024-11-25 15:58:07 +0100

    Update QTE.cs

--- OMH_Proto/Assets/[Project]/Scripts/Gameplay/QTE/QTE.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Gameplay/QTE/QTE.cs
@@ -1,23 +1,47 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class QTE : MonoBehaviour
 {
-    [SerializeField] private GameEvent _onQTEStart;
-    [SerializeField] private GameEvent _onQTEEnd;
-    private int _currentIndex = 0;
-    private List<Vector2> _currentDirectionSequence = new List<Vector2>();
+    [SerializeField] private UnityEvent<bool> _onInputEvent;
+    [SerializeField] private UnityEvent _onQTEWin;
+    [SerializeField] private UnityEvent _onQTEKill;
 
-    public void Play(List<Vector2> directionSequence)
+    private List<Vector2> _directionSequence = new List<Vector2>();
+    private int _index = 0;
+    private QTEUI _qteUi;
+
+    private void Start()
     {
-        _currentDirectionSequence = directionSequence;
+        _qteUi = GetComponent<QTEUI>();
+    }
+
+    public void StartQTE(List<Vector2> directionSequence)
+    {
+        _index = 0;
+        _directionSequence = directionSequence;
+        _qteUi.ActivateUI(_directionSequence);
     }
 
     private void PlayInput(Vector2 inputDirection)
     {
-        _toDoInInput.Invoke(true);
+        if (_directionSequence[_index] == inputDirection)
+        {
+            if (_index + 1 > _directionSequence.Count)
+            {
+                
+                return;
+            }
+
+            _onInputEvent.Invoke(true);
+            _index++;
+        }
+        else
+        {
+            _onInputEvent.Invoke(false);
+        }
     }
 
     private void OnInputDirection(InputValue value)
