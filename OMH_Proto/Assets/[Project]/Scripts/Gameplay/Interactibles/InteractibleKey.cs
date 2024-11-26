f24a1c33a4e5708d418b19e01fc15e39c41f0bf5
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2024-11-26 13:06:01 +0100

    inter

--- OMH_Proto/Assets/[Project]/Scripts/Gameplay/Interactibles/InteractibleKey.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Gameplay/Interactibles/InteractibleKey.cs
@@ -2,29 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractibleKey : Interactibles
+public class InteractibleKey : Interactible
 {
     [SerializeField] private GameEvent _getKey;
 
-    public override void Interact()
-    {
-        if (!_isPlayerInRange) return;
+    // public override void Interact()
+    // {
+    //     if (!_isPlayerInRange) return;
 
-        FloatVariable keyValue = new FloatVariable();
+    //     FloatVariable keyValue = new FloatVariable();
 
-        for (int i = 0; i < _infosManager._variables.Count; i++)
-        {
-            if (_infosManager._variables[i]._variableName == "Key")
-            {
-                keyValue = _infosManager._variables[i]._floatVariable;
-                break;
-            }
-        }
+    //     for (int i = 0; i < _infosManager._variables.Count; i++)
+    //     {
+    //         if (_infosManager._variables[i]._variableName == "Key")
+    //         {
+    //             keyValue = _infosManager._variables[i]._floatVariable;
+    //             break;
+    //         }
+    //     }
 
-        keyValue.Value += 1f;
+    //     keyValue.Value += 1f;
 
-        _getKey.Raise();
+    //     _getKey.Raise();
 
-        Destroy(gameObject);
-    }
+    //     Destroy(gameObject);
+    // }
 }
--- OMH_Proto/Assets/[Project]/Scripts/Gameplay/Interactibles/InteractibleSyringe.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Gameplay/Interactibles/InteractibleSyringe.cs
@@ -2,29 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractibleSyringe : Interactibles
+public class InteractibleSyringe : Interactible
 {
     [SerializeField] private GameEvent _getSyringe;
 
-    public override void Interact()
-    {
-        if (!_isPlayerInRange) return;
+    // public override void Interact()
+    // {
+    //     if (!_isPlayerInRange) return;
 
-        FloatVariable syringeValue = new FloatVariable();
+    //     FloatVariable syringeValue = new FloatVariable();
 
-        for (int i = 0; i < _infosManager._variables.Count; i++)
-        {
-            if (_infosManager._variables[i]._variableName == "Syringe")
-            {
-                syringeValue = _infosManager._variables[i]._floatVariable;
-                break;
-            }
-        }
+    //     for (int i = 0; i < _infosManager._variables.Count; i++)
+    //     {
+    //         if (_infosManager._variables[i]._variableName == "Syringe")
+    //         {
+    //             syringeValue = _infosManager._variables[i]._floatVariable;
+    //             break;
+    //         }
+    //     }
 
-        syringeValue.Value += 1f;
+    //     syringeValue.Value += 1f;
 
-        _getSyringe.Raise();
+    //     _getSyringe.Raise();
 
-        Destroy(gameObject);
-    }
+    //     Destroy(gameObject);
+    // }
 }
