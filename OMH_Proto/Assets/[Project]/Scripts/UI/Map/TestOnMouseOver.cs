28b8be00052d4c458a315204c477ff4a5057088d
Dragomal <115795428+Dragomal@users.noreply.github.com>
2024-11-28 13:39:18 +0100

    Add Blocking map / Door Couloir

--- OMH_Proto/Assets/[Project]/Scripts/UI/Map/TestOnMouseOver.cs
+++ OMH_Proto/Assets/[Project]/Scripts/UI/Map/TestOnMouseOver.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 
-public class TestOnMouseOver : MonoBehaviour ,IPointerMoveHandler
+public class TestOnMouseOver : MonoBehaviour ,IPointerClickHandler
 {
     [SerializeField] private Camera _mapCamera;
 
+    [SerializeField] private LayerMask _layerMask;
     private Vector3 worldPos;
     
-    public void OnPointerMove(PointerEventData eventData)
+    public void OnPointerClick(PointerEventData eventData)
     {
-        // print(transform.InverseTransformPoint(eventData.position));
-
         float tempFloatx = Mathf.InverseLerp(-300, 300, transform.InverseTransformPoint(eventData.position).x);
         float tempFloaty = Mathf.InverseLerp(-300, 300, transform.InverseTransformPoint(eventData.position).y);
 
@@ -23,12 +22,15 @@ public class TestOnMouseOver : MonoBehaviour ,IPointerMoveHandler
 
         worldPos = _mapCamera.transform.TransformPoint(new Vector3(x, y, 0));
 
-        Physics.Raycast(worldPos, Vector3.down, out RaycastHit ray, 100, 7);
+        Physics.Raycast(worldPos, Vector3.down, out RaycastHit ray, 100, _layerMask);
         if (!ray.collider) return;
         Debug.DrawRay(worldPos, Vector3.down, Color.green);
-        
-        print(ray.collider.name);
-        
+
+        IMapClickable mapClickable = ray.collider.GetComponent<IMapClickable>();
+        if (mapClickable != null)
+        {
+            mapClickable.OnClick();
+        }
     }
 
     public void OnDrawGizmos()
