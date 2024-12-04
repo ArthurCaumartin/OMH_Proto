79ed4a0035fd5afc2239551a32f20fa533f1a598
Duretete Martin <115795428+Dragomal@users.noreply.github.com>
2024-12-04 14:31:01 +0100

    Add Victory Event

--- OMH_Proto/Assets/[Project]/Scripts/GameManager.cs
+++ OMH_Proto/Assets/[Project]/Scripts/GameManager.cs
@@ -7,29 +7,35 @@ public enum GameState
     NotSet,
     Exploration,
     Defense,
+    Victory,
 }
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private GameEvent _defenseStartEvent;
+    [SerializeField] private GameEvent _defenseStartEvent, _victoryGameEvent;
     [Space]
     [SerializeField] private FloatReference _gameTime;
-    [SerializeField] private FloatReference _explorationDuration;
+    [SerializeField] private FloatReference _explorationDuration, _defenseDuration;
     [SerializeField] private NavMeshSurface _navMesh;
     private GameState _currentGameState;
 
     private void Start()
     {
+        SetGameState(GameState.Exploration);
         _gameTime.Value = 0;
     }
 
     private void Update()
     {
         _gameTime.Value += Time.deltaTime;
-        if (_gameTime.Value > _explorationDuration.Value)
+        if (_gameTime.Value > _defenseDuration.Value && _currentGameState == GameState.Defense)
         {
-            // print("Set Defense !");
+            SetGameState(GameState.Victory);
+        }
+        else if (_gameTime.Value > _explorationDuration.Value && _currentGameState == GameState.Exploration)
+        {
             SetGameState(GameState.Defense);
+            _gameTime.Value = 0;
         }
     }
 
@@ -45,6 +51,9 @@ public class GameManager : MonoBehaviour
 
             case GameState.Defense:
                 _defenseStartEvent.Raise();
+                break;
+            case GameState.Victory:
+                _victoryGameEvent.Raise();
                 break;
         }
     }
