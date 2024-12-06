390412de0d89a15f52ee7b6c8ad4708d119ebdb5
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2024-12-06 10:59:16 +0100

    Update GameEventListener.cs

--- OMH_Proto/Assets/[Project]/Scripts/Events/GameEventListener.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Events/GameEventListener.cs
@@ -7,10 +7,6 @@ public class GameEventListener : MonoBehaviour
 {
     [SerializeField] private GameEvent _event;
     [SerializeField] private UnityEvent<bool> _response;
-    private List<Action> _actionList;
-
-
-
 
     private void OnEnable() { _event.RegisterListener(this); }
     private void OnDisable() { _event.UnRegisterListener(this); }
@@ -19,12 +15,6 @@ public class GameEventListener : MonoBehaviour
     {
         _response.Invoke(eventValue);
 
-        for (int i = 0; i < _actionList.Count; i++)
-            _actionList[i].Invoke();
-    }
 
-    public void SubAction(Action action)
-    {
-        if (!_actionList.Contains(action)) _actionList.Add(action);
     }
 }
