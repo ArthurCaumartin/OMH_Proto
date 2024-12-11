1a6f25c843fdd88c0e19a29332540cf37254f5be
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2024-12-11 15:05:59 +0100

    Update DecalControler.cs

--- OMH_Proto/Assets/[Project]/Scripts/Visual/DecalControler.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Visual/DecalControler.cs
@@ -18,7 +18,6 @@ public class DecalControler : MonoBehaviour
     private void Update()
     {
         _currentTime += Time.deltaTime;
-        //! sorry future me
         _projector.fadeFactor = Mathf.Lerp(1, 0, _fadeOnLife.Evaluate(Mathf.InverseLerp(0, _lifeTime, _currentTime)));
         if(_currentTime >= _lifeTime) Destroy(gameObject);
     }
