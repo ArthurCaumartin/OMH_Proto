192713b93af341ef0dcd45cb24e2844c7bc00217
Valou <valentinsch@free.fr>
2025-01-13 16:04:19 +0100

    UpdateShieldAnne_et_MurDeDefense

---
+++ OMH_Proto/Assets/[Project]/Scripts/Shaders/ShieldBreaking.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class ShieldBreaking : MonoBehaviour
+
+
+
+//public void DestroyShield(){
+//    StartCoroutine(StartAnimation());
+//}
+
+//private IEnumerator StartAnimation(){
+    
+//    yield return new WaitForSecondes(1f);
+
+
+//}
