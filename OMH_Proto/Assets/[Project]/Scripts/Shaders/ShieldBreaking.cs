87b7940a184d82780533e1723173b7dff3fa1365
Valou <valentinsch@free.fr>
2025-01-27 16:47:25 +0100

    UpdateShaders+Code

--- OMH_Proto/Assets/[Project]/Scripts/Shaders/ShieldBreaking.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Shaders/ShieldBreaking.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.Compression;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,11 +9,16 @@ public class ShieldBreaking : MonoBehaviour
     [SerializeField] private Shield playerShield;
     [SerializeField] private Material _shieldMaterial;
 
+    public Color UpColor;
+    public Color BreakColor;
+    public float UpBlinkingSpeed;
+    public float BreakBlinkingSpeed;
+    
     public void Start()
     {
         playerShield._onShieldDown.AddListener(DestroyShield);
         playerShield._onShieldUp.AddListener(ShieldIsUp);
-
+        ShieldIsUp();
         // playerShield.ShieldDown += () => DestroyShield();
         // playerShield.ShieldUp += () => ShieldIsUp();
 
@@ -22,14 +28,18 @@ public class ShieldBreaking : MonoBehaviour
 
     public void DestroyShield()
     {
-        print("DestroyShield Shader");
-        _shieldMaterial.SetFloat("isShieldDown", 1);
+        print("DestroyShield");
+        _shieldMaterial.SetColor("_FlashingColor", BreakColor);
+        _shieldMaterial.SetFloat("_BlinkingSpeed", BreakBlinkingSpeed);
+        //FlashingColor.SetColor("_FlashingColor", 1);
     }
 
     public void ShieldIsUp()
     {
-        print("ShieldUp Shader");
-        _shieldMaterial.SetFloat("isShieldDown", 0); 
+        print("ShieldUp");
+        _shieldMaterial.SetColor("_FlashingColor", UpColor);
+        _shieldMaterial.SetFloat("_BlinkingSpeed", UpBlinkingSpeed);
+        //_FlashingColor = AlarmFlashingColor; 
     }
 }
 
