b0c80f2a1236cc64512be8955af9b922e63beea1
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-02-04 11:33:57 +0100

    add attack state

--- OMH_Proto/Assets/[Project]/Scripts/StateMachine/Mob/StateMachine_MobBase.cs
+++ OMH_Proto/Assets/[Project]/Scripts/StateMachine/Mob/StateMachine_MobBase.cs
@@ -6,12 +6,14 @@ public class StateMachine_MobBase : StateMachine
     [SerializeField] private State_Mob_Roam _roamState = new State_Mob_Roam();
     [SerializeField] private State_Mob_Spine _spineState = new State_Mob_Spine();
     [SerializeField] private State_Mob_Chase _chaseState = new State_Mob_Chase();
+    [SerializeField] private State_Mob_Attack _attackState = new State_Mob_Attack();
     private MobTargetFinder _targetFinder;
 
     public Transform Target { get => _target; }
     public State_Mob_Roam RoamState { get => _roamState; }
     public State_Mob_Spine SpineState { get => _spineState; }
     public State_Mob_Chase ChaseState { get => _chaseState; }
+    public State_Mob_Attack AttackState { get => _attackState; }
 
     private void Start()
     {
@@ -22,6 +24,7 @@ public class StateMachine_MobBase : StateMachine
         _roamState.Initialize(this);
         _spineState.Initialize(this);
         _chaseState.Initialize(this);
+        _attackState.Initialize(this);
 
         _currentState = _roamState;
     }
