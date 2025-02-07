de57e4ff63c30dd205941cf6d7fad509b46b1322
Duretete Martin <115795428+Dragomal@users.noreply.github.com>
2025-02-07 11:28:56 +0100

    Add functionning map and mini map with infos

---
+++ OMH_Proto/Assets/[Project]/Scripts/UI/Map/MapSwitch.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSwitch : MonoBehaviour
+{
+    [SerializeField] private GameObject _littleMap, _tallMap;
+    [SerializeField] private GameEvent _switchTallMapEvent, _switchLittleMapEvent;
+
+    private bool _isMapTall;
+    
+    public void OnOpenLeftMenu()
+    {
+        if (_isMapTall)
+        {
+            _tallMap.SetActive(false);
+            _littleMap.SetActive(true);
+            _isMapTall = false;
+            _switchLittleMapEvent.Raise();
+        }
+        else
+        {
+            _tallMap.SetActive(true);
+            _littleMap.SetActive(false);
+            _isMapTall = true;
+            _switchTallMapEvent.Raise();
+        }
+    }
+}
