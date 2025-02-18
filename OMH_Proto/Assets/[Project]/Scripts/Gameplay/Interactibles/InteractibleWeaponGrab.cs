4ff110965db2833f27aa96111574ab8bf3ecd7b7
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-02-18 13:29:51 +0100

    comment print

--- OMH_Proto/Assets/[Project]/Scripts/Gameplay/Interactibles/InteractibleWeaponGrab.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Gameplay/Interactibles/InteractibleWeaponGrab.cs
@@ -7,7 +7,6 @@ public class InteractibleWeaponGrab : Interactible
 
     public override void Interact(PlayerInteract playerInteract, out bool cancelIteraction)
     {
-        print("try grab weapon");
         cancelIteraction = false;
         playerInteract.GetComponent<WeaponControler>()?.AddWeapon(_weaponTograb);
         Destroy(gameObject);
