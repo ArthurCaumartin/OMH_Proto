d29694f76b0b35224ebc2da9e4d5b68bb5c4f850
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-02-19 15:11:00 +0100

    Update InteractibleChest.cs

--- OMH_Proto/Assets/[Project]/Scripts/Gameplay/Interactibles/InteractibleChest.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Gameplay/Interactibles/InteractibleChest.cs
@@ -4,26 +4,30 @@ using DG.Tweening;
 public class Chest_Shader_Controller : Interactible
 {
     [Space]
-    [SerializeField] private ParticleSystem _openParticle;
     [SerializeField] private GameEvent _onOpenChestEvent;
-    [SerializeField] private Transform _topPart;
     [SerializeField] private GameObject _mapPin;
+    [Space]
+    [SerializeField] private float _animationDuration = 1;
+    [SerializeField] private Transform _topPart;
+    [SerializeField] private ParticleSystem _openParticle;
+    // [SerializeField] private Light _light;
+    // [SerializeField] private float _lightIntensity = 5;
 
     public override void OnQTEWin()
     {
         _onOpenChestEvent.Raise();
-        
-        _topPart.DOLocalRotate(new Vector3(-195, 0, 0), .7f);
-        if(_openParticle)
+
+        _topPart.DOLocalRotate(new Vector3(-195, 0, 0), _animationDuration);
+        // _light.DOIntensity(_lightIntensity, _animationDuration / 2)
+        // .OnComplete(() => _light.DOIntensity(0, _animationDuration / 2));
+
+        if (_openParticle)
         {
             ParticleSystem p = Instantiate(_openParticle, transform);
             Destroy(p.gameObject, p.main.duration + 1);
         }
 
         _mapPin.SetActive(false);
-
         Destroy(this);
-        // BoxCollider boxCollider = GetComponent<BoxCollider>();
-        // boxCollider.enabled = false;
     }
 }
