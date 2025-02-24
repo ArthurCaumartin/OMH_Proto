982b61d9fb5e8396a361c05e71bf8d841a4f5102
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-02-24 13:32:05 +0100

    Add Upgradable call OnValidate + level overflow fix

--- OMH_Proto/Assets/[Project]/Scripts/Variables/Meta/UpgradeMeta.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Variables/Meta/UpgradeMeta.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Upgrade", menuName = "Upgrade Meta")]
 public class UpgradeMeta : ScriptableObject
 {
-    public int _upgradeCost;
     public string _upgradeName;
+    public int _upgradeCost;
     [TextArea] public string _upgradeDescription;
     public Sprite _upgradeIcon;
+    [Space]
+    public int currentLevel;
+    public List<FloatReference> levelValue;
+
+    public float GetUpgradeValue()
+    {
+        if (currentLevel > levelValue.Count - 1)
+            return levelValue[levelValue.Count - 1].Value;
+        return levelValue[currentLevel].Value;
+    }
+
+    private void OnValidate()
+    {
+        Upgradable.SetUpgradeValue();
+    }
 }
- 
