bc7a855d346cc516caa5766e31ccd7ce6e28a9d5
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-02-26 14:52:40 +0100

    add health bar to mob life again :))))))))))))))))))))))))))

--- OMH_Proto/Assets/[Project]/Scripts/Gameplay/GameLoop/Mob/MobLife.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Gameplay/GameLoop/Mob/MobLife.cs
@@ -5,17 +5,20 @@ using UnityEngine.Events;
 
 public class MobLife : MonoBehaviour, IDamageable
 {
-    [SerializeField] private FloatReference _mobHealth;
+    [SerializeField] private FloatReference _mobMaxHealth;
     [SerializeField] private Renderer _renderer;
-    private float _health;
+    private float _currentHealth;
     [SerializeField] private UnityEvent<MobLife, DamageType> _onDeathEvent;
     [SerializeField] private UnityEvent<GameObject, DamageType> _onDamageTakenEvent;
     public UnityEvent<GameObject, DamageType> OnDamageTakenEvent { get => _onDamageTakenEvent; }
     public UnityEvent<MobLife, DamageType> OnDeathEvent { get => _onDeathEvent; }
+    private HealthBar _healthBar;
 
     private void Start()
     {
-        _health = _mobHealth.Value;
+        _currentHealth = _mobMaxHealth.Value;
+        _healthBar = GetComponentInChildren<HealthBar>();
+        _healthBar?.SetFillAmount(1);
     }
 
     public void TakeDamages(GameObject damageDealer, float value, DamageType type)
@@ -26,8 +29,9 @@ public class MobLife : MonoBehaviour, IDamageable
             _onDamageTakenEvent.Invoke(damageDealer, type);
         }
 
-        _health -= value;
-        if (_health <= 0)
+        _currentHealth -= value;
+        _healthBar.SetFillAmount(Mathf.InverseLerp(0, _mobMaxHealth.Value, _currentHealth), true);
+        if (_currentHealth <= 0)
         {
             _onDeathEvent.Invoke(this, type);
             Death();
