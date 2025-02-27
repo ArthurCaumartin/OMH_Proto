a61d71c5ddf31ad91f32b102779bd9df9413eebf
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-02-27 10:11:03 +0100

    add rail condition in placable for color feedback

--- OMH_Proto/Assets/[Project]/Scripts/Gameplay/Placable/Placable.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Gameplay/Placable/Placable.cs
@@ -6,6 +6,7 @@ public class Placable : MonoBehaviour
     [SerializeField] public FloatReference cost;
     [SerializeField] private GameObject _prefabToPlace;
     [Space]
+    [SerializeField] private bool _needRailToPlace = false;
     [SerializeField] private string _colorParameterName = "_ObjectColor";
     [SerializeField] private float _transitionSpeed = 5;
     [SerializeField] private Color _canPlaceColor;
@@ -14,9 +15,12 @@ public class Placable : MonoBehaviour
     [HideInInspector] public bool placeOnCorridorRail = false;
     private List<GameObject> _blockObject = new List<GameObject>();
     private Renderer[] _rendererArray;
+    private bool _isOnRail;
 
     public GameObject PrefabToPlace { get => _prefabToPlace; }
     public bool CanBePlaced { get => _blockObject.Count == 0; }
+    public bool IsOnRail { set => _isOnRail = value; }
+    private Color _renderColorTarget;
 
     private void Start()
     {
@@ -31,11 +35,16 @@ public class Placable : MonoBehaviour
     private void SetMeshsColor()
     {
         if (_rendererArray.Length == 0) return;
-        Color renderColorTarget = _blockObject.Count == 0 ? _canPlaceColor : _canNotPlaceColor;
+
+        _renderColorTarget = _blockObject.Count == 0 ? _canPlaceColor : _canNotPlaceColor;
+
+        if (_needRailToPlace)
+            _renderColorTarget = _isOnRail ? (_blockObject.Count == 0 ? _canPlaceColor : _canNotPlaceColor) : _canNotPlaceColor;
+
         foreach (var item in _rendererArray)
         {
             item.material.SetColor(_colorParameterName
-            , Color.Lerp(item.material.GetColor(_colorParameterName), renderColorTarget, Time.deltaTime * _transitionSpeed));
+            , Color.Lerp(item.material.GetColor(_colorParameterName), _renderColorTarget, Time.deltaTime * _transitionSpeed));
         }
     }
 
