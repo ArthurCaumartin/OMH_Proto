4353eb33e093536706e351be1809cb7298a827b6
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-02-27 11:51:56 +0100

    Update Health.cs

--- OMH_Proto/Assets/[Project]/Scripts/Gameplay/GameLoop/Health.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Gameplay/GameLoop/Health.cs
@@ -13,18 +13,20 @@ public class Health : MonoBehaviour, IDamageable
     [SerializeField] private UnityEvent<float> _onDamageTaken;
     private HealthBar _healthBar;
 
+    public UnityEvent<float> OnDamageTaken { get => _onDamageTaken; }
+
     private void Start()
     {
         _currentHealth.Value = _maxHealth.Value;
         _healthBar = GetComponentInChildren<HealthBar>();
-        _healthBar?.SetFillAmount(Mathf.InverseLerp(0, _maxHealth.Value, _currentHealth.Value), _currentHealth.Value != _maxHealth.Value);
+        _healthBar?.SetFillAmount(GetHealtRatio(), _currentHealth.Value != _maxHealth.Value);
     }
 
     public void TakeDamages(GameObject damageDealer, float damageAmount, DamageType type = DamageType.Unassigned)
     {
         _onDamageTaken.Invoke(damageAmount);
         _currentHealth.Value -= damageAmount;
-        _healthBar?.SetFillAmount(Mathf.InverseLerp(0, _maxHealth.Value, _currentHealth.Value));
+        _healthBar?.SetFillAmount(GetHealtRatio());
         if (_currentHealth.Value <= 0) Destroy(gameObject);
     }
 
@@ -32,6 +34,11 @@ public class Health : MonoBehaviour, IDamageable
     {
         _currentHealth.Value += healAmount;
         _currentHealth.Value = Mathf.Clamp(_currentHealth.Value, -1000, _maxHealth.Value);
-        _healthBar?.SetFillAmount(Mathf.InverseLerp(0, _maxHealth.Value, _currentHealth.Value));
+        _healthBar?.SetFillAmount(GetHealtRatio());
+    }
+
+    public float GetHealtRatio()
+    {
+        return Mathf.InverseLerp(0, _maxHealth.Value, _currentHealth.Value);
     }
 }
