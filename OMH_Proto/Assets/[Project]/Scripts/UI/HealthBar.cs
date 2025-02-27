f99136fe446f1068c241722d6edea364ebc4cf03
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-02-27 14:25:57 +0100

    Add delay before hide health bar

--- OMH_Proto/Assets/[Project]/Scripts/UI/HealthBar.cs
+++ OMH_Proto/Assets/[Project]/Scripts/UI/HealthBar.cs
@@ -5,21 +5,40 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Canvas _canvas;
     [SerializeField] private Image _healthBarImage;
+    float _delayBeforeFade;
+    private float _ratioTarget;
 
-    public void SetFillAmount(float toSet, bool enableImage = true)
+    private void Start()
     {
-        if (toSet > .99)
+        _ratioTarget = 1;
+        Enable(false);
+    }
+
+    void Update()
+    {
+        if (_ratioTarget > .95f)
         {
-            _canvas.gameObject.SetActive(false);
+            _delayBeforeFade += Time.deltaTime;
+            if (_delayBeforeFade > 2)
+                Enable(false);
             return;
         }
-        
-        _canvas.gameObject.SetActive(enableImage);
-        // _healthBarImage.fillAmount = toSet;
-        // print(toSet);
-        RectTransform rt = _healthBarImage.transform as RectTransform;
 
-        rt.offsetMin = new Vector2(-toSet, rt.offsetMin.y);
-        rt.offsetMax = new Vector2(toSet, rt.offsetMax.y);
+        Enable(true);
+        _delayBeforeFade = 0;
+    }
+
+    public void SetFillAmount(float toSet)
+    {
+        RectTransform rt = _healthBarImage.transform as RectTransform;
+        _ratioTarget = toSet;
+
+        rt.offsetMin = new Vector2(-_ratioTarget, rt.offsetMin.y);
+        rt.offsetMax = new Vector2(_ratioTarget, rt.offsetMax.y);
+    }
+
+    private void Enable(bool value)
+    {
+        _canvas.enabled = value;
     }
 }
