e9721f9850bd9a4bd6428e9ce339e9de04d6fd3d
Duretete Martin <115795428+Dragomal@users.noreply.github.com>
2025-02-27 11:19:02 +0100

    Add Blur in background menu and inventory

--- OMH_Proto/Assets/[Project]/Scripts/PauseMenu.cs
+++ OMH_Proto/Assets/[Project]/Scripts/PauseMenu.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _pauseMenu;
+    [SerializeField] private Volume _volume;
     
-    public void OnPauseMenu()
+    public void Pause()
     {
         _pauseMenu.SetActive(true);
         Time.timeScale = 0;
+        _volume.weight = 1;
     }
     
-    public void OnResumeMenu()
+    public void ResumeMenu()
     {
         _pauseMenu.SetActive(false);
         Time.timeScale = 1;
+        _volume.weight = 0;
     } 
 }
