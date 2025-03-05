87f04a52b60a36852353b66a5ec8bc6cf3625d1d
Duretete Martin <115795428+Dragomal@users.noreply.github.com>
2025-03-05 14:37:30 +0100

    Add golden Chest

--- OMH_Proto/Assets/[Project]/Scripts/Gameplay/Interactibles/InteractibleChest.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Gameplay/Interactibles/InteractibleChest.cs
@@ -13,15 +13,34 @@ public class Chest_Shader_Controller : Interactible
     [SerializeField] private float _animationDuration = 1;
     [SerializeField] private Transform _topPart;
     [SerializeField] private ParticleSystem _openParticle;
+    [Space]
+    [SerializeField] private GameObject _goldenChestPrefab;
+    
     private bool _isOpen = false;
 
+    public static bool _isGoldenChest; 
+    private List<Chest_Shader_Controller> _chestControllers = new List<Chest_Shader_Controller>();
+
+    void Awake()
+    {
+        _isGoldenChest = false;
+    }
+    
     void Start()
     {
+        base.Start();
         Object[] chest = FindObjectsOfType(typeof(Chest_Shader_Controller));
-        for (int i = 0; i < chest.Length; i++)
+        foreach (Chest_Shader_Controller chestShader in chest)
         {
-            
+            _chestControllers.Add(chestShader);
         }
+        int tempInt = Random.Range(0, _chestControllers.Count);
+
+        print("Before Golden Chest");
+        if (_isGoldenChest) return;
+        print("Golden Chest");
+        _chestControllers[tempInt].TransformationGoldChest();
+        _isGoldenChest = true;
     }
     
     public override void Interact(PlayerInteract playerInteract, out bool cancelIteraction)
@@ -46,6 +65,12 @@ public class Chest_Shader_Controller : Interactible
         gameObject.layer = LayerMask.NameToLayer("Default");
         // Destroy(gameObject);
         // GetComponent<BoxCollider>().enabled = false;
+    }
+
+    public void TransformationGoldChest()
+    {
+        Instantiate(_goldenChestPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 
     private IEnumerator GetLoot(float delay)
