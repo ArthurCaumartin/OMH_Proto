73cc12c046260a36b8dcb1ee900f92f418598198
Duretete Martin <115795428+Dragomal@users.noreply.github.com>
2025-03-17 14:01:24 +0100

    Push map cassé

--- OMH_Proto/Assets/[Project]/Scripts/UI/Map/MapFogOfWar.cs
+++ OMH_Proto/Assets/[Project]/Scripts/UI/Map/MapFogOfWar.cs
@@ -14,7 +14,9 @@ public class MapFogOfWar : MonoBehaviour
     [SerializeField] private Texture2D _texture;
     [SerializeField] private int _circleRadius;
     
-    private Vector2 relativePosPlayer; 
+    private Vector2 relativePosPlayer;
+
+    public Color _bgColor, _colorisation;
 
     private void Awake()
     {
@@ -22,7 +24,7 @@ public class MapFogOfWar : MonoBehaviour
         {
             for (int j = 0; j < _texture.width; j++)
             {
-                _texture.SetPixel(j, i, Color.black);
+                _texture.SetPixel(j, i, _bgColor);
             }
         }
         _texture.Apply();
@@ -36,9 +38,14 @@ public class MapFogOfWar : MonoBehaviour
     //It's working but it cost a lot
     private void Update()
     {
-        TracePixel(GetPos(), _circleRadius, new Color(0,0,0,0));
-        TracePixel(GetPos(), _circleRadius - 1, new Color(0,0,0,0));
-        TracePixel(GetPos(), _circleRadius - 2, new Color(0,0,0,0));
+        // TracePixel(GetPos(), _circleRadius, new Color(0,0,0,0));
+        // TracePixel(GetPos(), _circleRadius - 1, new Color(0,0,0,0));
+        // TracePixel(GetPos(), _circleRadius - 2, new Color(0,0,0,0));
+        
+        
+        TracePixel(GetPos(), _circleRadius, _colorisation);
+        TracePixel(GetPos(), _circleRadius - 1, _colorisation);
+        TracePixel(GetPos(), _circleRadius - 2, _colorisation);
     }
 
     public void TracePixelRoom(Transform roomPos, int radius)
