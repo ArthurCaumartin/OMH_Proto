4914e25d37b2085fe1005ef966c6c52c118009ea
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-03-24 14:42:32 +0100

    Add object oclusion check

--- OMH_Proto/Assets/[Project]/Scripts/Visual/DisolveEffect.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Visual/DisolveEffect.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using System.Security.Cryptography;
 using DG.Tweening;
 using UnityEngine;
 
@@ -5,33 +7,61 @@ public class DisolveEffect : MonoBehaviour
 {
     [SerializeField] private string _parameterName = "_Alpha";
     [SerializeField] private float _disolveDuration = 1;
+    [SerializeField] private bool _initialVisivbility = false;
+    [Space]
     [SerializeField] private Health _health;
-
+    [Space]
+    [SerializeField] private Transform _objectToSeeThrought;
+    [SerializeField] private float _transitionSpeed = 5;
     private Renderer[] _rendererArray;
-
+    private Transform _cameraTransform;
+    private bool _isBehind = false;
 
     private void Start()
     {
+        _cameraTransform = Camera.main.transform;
         _rendererArray = GetComponentsInChildren<Renderer>();
 
         _health?.SetDelayBeforDestroy(_disolveDuration);
         _health?.OnDeathEvent.AddListener(() => Disolve(true));
 
-        Disolve(false);
+        Disolve(_initialVisivbility);
     }
 
+    void Update()
+    {
+        if (!_objectToSeeThrought) return;
+
+        Vector3 dir = (_cameraTransform.position - _objectToSeeThrought.position).normalized;
+        RaycastHit[] hits = Physics.RaycastAll(_objectToSeeThrought.position, dir, 10);
+
+        _isBehind = false;
+        foreach (var item in hits)
+        {
+            if (item.collider.gameObject == gameObject)
+            {
+                _isBehind = true;
+                break;
+            }
+        }
+
+        float current = _rendererArray[0].material.GetFloat(_parameterName);
+        SetMaterialsParameter(Mathf.Lerp(current, _isBehind ? 0 : 1, Time.deltaTime * _transitionSpeed));
+    }
 
     public void Disolve(bool isHiding)
     {
         // print("ksedfrgbj");
-        foreach (var item in _rendererArray)
-            item.material.SetFloat(_parameterName, isHiding ? 1 : 0);
-
+        SetMaterialsParameter(isHiding ? 1 : 0);
         DOTween.To((time) =>
         {
-            foreach (var item in _rendererArray)
-                item.material.SetFloat(_parameterName, time);
-
+            SetMaterialsParameter(time);
         }, isHiding ? 1 : 0, isHiding ? 0 : 1, _disolveDuration);
+    }
+
+    private void SetMaterialsParameter(float time)
+    {
+        foreach (var item in _rendererArray)
+            item.material.SetFloat(_parameterName, time);
     }
 }
