bea9b11e10135336275a24adb7380d5be209df5d
Valou <valentinsch@free.fr>
2025-03-26 16:46:44 +0100

    VIdageCuve

---
+++ OMH_Proto/Assets/[Project]/Scripts/Visual/Shaders/MoveOnDefenseTime.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnDefenseTime : MonoBehaviour
+{
+    [SerializeField] private Transform startPos;
+    [SerializeField] private Transform endPos;
+    [SerializeField] private GameObject MovingObject;
+    [SerializeField] private SpawnManager _spawnManager;
+    [SerializeField] private FloatReference _defenseDuration;
+    public bool Active;
+    private float _timePassed;
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    //     PositionInitiale = MovingObject.transform.position;
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_spawnManager._defenseAsStarted || Active) ObjectMovement();
+    }
+
+    public void ObjectMovement()
+    {
+        _timePassed += Time.deltaTime;
+        MovingObject.transform.position = Vector3.Lerp(startPos.position, endPos.position, Mathf.InverseLerp(0, _defenseDuration.Value, _timePassed));
+    }
+}
