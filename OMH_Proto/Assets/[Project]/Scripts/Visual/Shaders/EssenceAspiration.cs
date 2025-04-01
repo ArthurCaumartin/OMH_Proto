743eb6d5b52a05dd3714bb0551c1a7480159199e
Duretete Martin <115795428+Dragomal@users.noreply.github.com>
2025-04-01 17:37:01 +0200

    Squashed commit of the following:
    commit df4beecab984e4e83ea24876ef8e602df95d1219
    Author: Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
    Date:   Tue Apr 1 16:54:57 2025 +0200
    
        Update RareList.asset
    
    commit 4cd4e3ceebf55012fbf525cc5fc3bc7b59db9988
    Author: Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
    Date:   Tue Apr 1 16:53:34 2025 +0200
    
        move camera on start
    
    commit 0d1f32277e5e5e673f5bde60d192904b4d5d35a1
    Author: Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
    Date:   Tue Apr 1 16:53:02 2025 +0200
    
        Fixe item drop
    
    commit 55aacdc5038275cd74e8d25f5471f511a5885cbc
    Author: Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
    Date:   Tue Apr 1 16:49:42 2025 +0200
    
        fix
    
    commit 8748c6b1e040dd71ad70c4a4b2c3c02e08c79038
    Merge: aa57688a e9b7453b
    Author: Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
    Date:   Tue Apr 1 16:47:29 2025 +0200
    
        Merge branch 'V2_IntéGD' into V2_Dev
    
    commit aa57688a14ef5052705e0387c24bbe2de747a885
    Author: Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
    Date:   Tue Apr 1 16:47:05 2025 +0200
    
        Update Avril_Gameplay.unity
    
    commit ef354eb424df0b82ed1feb051ba33235b84000e4
    Author: Valou <valentinsch@free.fr>
    Date:   Tue Apr 1 16:43:36 2025 +0200
    
        Fix_Pcen
    
        Fix de Unity qui fait le bougon à pas vouloir prendre le script lors de l'aspiration de l'essence
    
    commit 3ee53f7ba08c6b5a34059e8573d95fc77b73ad07
    Merge: 9286f266 75506486
    Author: Valou <valentinsch@free.fr>
    Date:   Tue Apr 1 16:04:26 2025 +0200
    
        Merge branch 'V2_Dev' into V2_Shader
    
    commit 9286f26645e20697ca14a835a997090bb35d25e9
    Author: Valou <valentinsch@free.fr>
    Date:   Tue Apr 1 16:04:17 2025 +0200
    
        we
    
    commit 755064860bca14ad567d6852d6816964b9ad02e3
    Author: Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
    Date:   Tue Apr 1 16:03:57 2025 +0200
    
        shader stuff
    
    commit 4a77ed3529249eaad58a06fd740ccdcd3ea85b46
    Merge: 80630d2e 907166db
    Author: Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
    Date:   Tue Apr 1 15:20:46 2025 +0200
    
        Merge branch 'V2_IntéGD' into V2_Dev
    
    commit 3d71f9369fb08663352d4117650691fc0c6f3a6b
    Merge: 5a615bec 3ca3e842
    Author: Valou <valentinsch@free.fr>
    Date:   Tue Apr 1 14:05:23 2025 +0200
    
        Merge branch 'V2_Loic' into V2_Shader
    
    commit 3ca3e84289b9874f209caaac13aa2f2d4cb8e4b0
    Author: Drackt <GunsCranell@gmail.com>
    Date:   Tue Apr 1 13:30:40 2025 +0200
    
        fix centrimat et light tripod

--- OMH_Proto/Assets/[Project]/Scripts/Visual/Shaders/EssenceAspiration.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Visual/Shaders/EssenceAspiration.cs
@@ -8,24 +8,25 @@ public class EssenceAspiration : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private SpawnManager _spawnManager;
     [SerializeField] private bool _active;
-    [SerializeField] private Material _essence;
-     private float _vitesseAspiration = 0;
-     [SerializeField] private float _transitionTime;
-
+    [SerializeField] int _index = 1;
+    public float _vitesseAspiration = 0;
+    [SerializeField] private float _speed;
+    private Renderer _renderer;
     void Start()
     {
-        _essence.SetFloat("_aspiration", 0);
+        _renderer = GetComponent<Renderer>();
+        //_essence.SetFloat("_aspiration", 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(_spawnManager._defenseAsStarted || _active) 
-       {
-            DOTween.To(() => _vitesseAspiration, x => _vitesseAspiration = x, 1f, _transitionTime);
-            _essence.SetFloat("_aspiration", _vitesseAspiration);
-            print (_vitesseAspiration);
-       }
+        if (_spawnManager._defenseAsStarted || _active)
+        {
+            _vitesseAspiration += Time.deltaTime * _speed;
+            _vitesseAspiration = Mathf.Clamp(_vitesseAspiration, 0, 1);
+            _renderer.materials[0].SetFloat ("_aspiration", _vitesseAspiration);
+            // print(_vitesseAspiration);
+        }
     }
-    
 }
