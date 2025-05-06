b22f413c365c098c923e044ae76a39f1a2914b7e
Duretete Martin <115795428+Dragomal@users.noreply.github.com>
2025-05-06 13:30:11 +0200

    Push canvas prefab

--- OMH_Proto/Assets/[Project]/Scripts/FadeInStartScene.cs
+++ OMH_Proto/Assets/[Project]/Scripts/FadeInStartScene.cs
@@ -11,7 +11,7 @@ public class FadeInStartScene : MonoBehaviour
     void Start()
     {
         _image = GetComponent<Image>();
-        
+        _image.color = new Color(0, 0, 0, 1);
         _image.DOFade(0, 1f);
     }
 
