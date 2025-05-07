51b37310bacd184a279eb9d2e6746f350793b17c
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-05-07 11:14:48 +0200

    update loading

---
+++ OMH_Proto/Assets/[Project]/Scripts/Tools/SlideAnimator.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class SlideAnimator : MonoBehaviour
+{
+    // [SerializeField] private TimeLine;
+
+    private bool _isSeekDone = false;
+
+
+
+
+
+
+
+
+
+
+}
