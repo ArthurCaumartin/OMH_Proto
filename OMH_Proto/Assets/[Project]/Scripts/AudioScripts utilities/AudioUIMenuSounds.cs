e8bebc1db471e9124b87303c8ff8ee725d27b669
Kygen-Sondidier <145322512+Kygen-Sondidier@users.noreply.github.com>
2025-05-08 16:15:58 +0200

    chore(audio): added audio events for UI sounds
    modified Button.prefab to include sound script
    edited AudioUIMenuSounds.cs

--- OMH_Proto/Assets/[Project]/Scripts/AudioScripts utilities/AudioUIMenuSounds.cs
+++ OMH_Proto/Assets/[Project]/Scripts/AudioScripts utilities/AudioUIMenuSounds.cs
@@ -5,10 +5,40 @@ using UnityEngine;
 
 public class AudioUIMenuSounds : MonoBehaviour
 {
-    [SerializeField] public AK.Wwise.Event _AssociatedUISound;
+    [SerializeField] public AK.Wwise.Event _Confirm;
+    [SerializeField] public AK.Wwise.Event _Back;
+    [SerializeField] public AK.Wwise.Event _Switch_perks;
+    [SerializeField] public AK.Wwise.Event _Error;
+    [SerializeField] public AK.Wwise.Event _Hoover;
+    [SerializeField] public AK.Wwise.Event _Select;
+    [SerializeField] public AK.Wwise.Event _Start;
 
-    public void OnUIClick()
+   public void OnConfirm()
     {
-        _AssociatedUISound.Post(gameObject);
+        _Confirm.Post(gameObject);
+    }
+    public void OnGoBack()
+    {
+        _Back.Post(gameObject);
+    }
+    public void OnSwitchPerks()
+    {
+        _Switch_perks.Post(gameObject);
+    }
+    public void OnError()
+    { 
+        _Error.Post(gameObject);
+    }
+    public void OnHoover()
+    {
+        _Hoover.Post(gameObject);
+    }
+    public void OnSelect()
+    {
+        _Select.Post(gameObject);
+    }
+    public void OnStartGame()
+    {
+        _Start.Post(gameObject);
     }
 }
