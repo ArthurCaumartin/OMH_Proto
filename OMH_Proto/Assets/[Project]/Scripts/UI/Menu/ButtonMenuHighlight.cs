5653ce7d029e2c5c0837cc6e9f5ab172cff2de23
Kygen-Sondidier <145322512+Kygen-Sondidier@users.noreply.github.com>
2025-05-13 22:07:06 +0200

    chore(audio): mapped sound on hoover menu elements
    Sound is now playing when user stays long enough on menu's buttons

--- OMH_Proto/Assets/[Project]/Scripts/UI/Menu/ButtonMenuHighlight.cs
+++ OMH_Proto/Assets/[Project]/Scripts/UI/Menu/ButtonMenuHighlight.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -14,17 +15,24 @@ public class ButtonMenuHighlight : Selectable, IPointerEnterHandler, IPointerExi
     [SerializeField] private Image _image;
     [SerializeField] private TextMeshProUGUI _text;
     
-    [SerializeField] private UnityEvent _onEnter;
     [SerializeField] private UnityEvent _onClick;
+
+    [SerializeField] public AK.Wwise.Event _enterAreaSound;
+    [SerializeField] private float _timeBeforeTrigSound = 0.14f;
+    public bool _cursorIsIn;
+    private IEnumerator _myCoroutine;
     
     public void OnPointerEnter(PointerEventData eventData)
     {
-        _onEnter.Invoke();
+        _cursorIsIn = true;
+        DelegateSoundHoover();
         _image.sprite = _highLightSprite;
         _text.color = _highLightColor;
     }
     public void OnPointerExit(PointerEventData eventData)
     {
+        _cursorIsIn = false;
+        if (_myCoroutine != null) StopCoroutine(_myCoroutine);
         _image.sprite = _normalSprite;
         _text.color = _normalColor;
     }
@@ -34,5 +42,25 @@ public class ButtonMenuHighlight : Selectable, IPointerEnterHandler, IPointerExi
         _onClick.Invoke();
         _image.sprite = _normalSprite;
         _text.color = _normalColor;
+    }
+    public void DelegateSoundHoover()
+    {
+        if (_myCoroutine != null) StopCoroutine(_myCoroutine);
+
+        if (_cursorIsIn)
+        {
+            _myCoroutine = IsInHoover();
+            StartCoroutine(_myCoroutine);
+        }
+
+    }
+    private IEnumerator IsInHoover()
+    {
+        yield return new WaitForSeconds(_timeBeforeTrigSound);
+        if (_cursorIsIn )
+        {
+            _enterAreaSound.Post(gameObject);
+        }
+        
     }
 }
