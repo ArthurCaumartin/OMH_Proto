ad560085b8783e8071bb5505bb83e3bc5ed63c60
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-05-28 15:42:21 +0200

    interactible feedback EEEE (disable for now)

--- OMH_Proto/Assets/[Project]/Scripts/Gameplay/Interactibles/Interactible.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Gameplay/Interactibles/Interactible.cs
@@ -1,16 +1,18 @@
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Interactible : MonoBehaviour
 {
     [SerializeField] private QTE _qte;
     public QTE QTE { get => _qte; }
     public bool HaveQTE { get => _qte; }
-    
+
     public string _textToInteract;
+    [SerializeField] private Canvas _interactText;
     [SerializeField] private List<MeshRenderer> _meshRenderersToOutline;
     [SerializeField] float _timeToVerifyOutline = 0.3f;
-     
+
     private float _timer;
     private bool _isOutlined;
 
@@ -23,6 +25,7 @@ public class Interactible : MonoBehaviour
             _qte.OnKill.AddListener(OnQTEKill);
         }
 
+        EnableFeedback(false);
     }
 
     public virtual void Interact(PlayerInteract playerInteract, out bool cancelIteraction)
@@ -46,28 +49,40 @@ public class Interactible : MonoBehaviour
         // print("INTERACTIBLE : QTE kill !");
     }
 
-    public void OutlineInteractible()
+    public void EnableInteractFeedback()
     {
         _isOutlined = true;
         _timer = 0;
-        for (int i = 0; i < _meshRenderersToOutline.Count; i++)
-        {
-            _meshRenderersToOutline[i].materials[_meshRenderersToOutline[i].materials.Length - 1].SetFloat("_outlineAlpha", 1);
-        }
+        EnableFeedback(true);
+        // for (int i = 0; i < _meshRenderersToOutline.Count; i++)
+        // {
+        //     _interactText.transform.DOScale(Vector3.one, .2f).SetEase(Ease.InOutBounce);
+        //     _meshRenderersToOutline[i].materials[_meshRenderersToOutline[i].materials.Length - 1].SetFloat("_outlineAlpha", 1);
+        // }
     }
 
     public void Update()
     {
         if (!_isOutlined) return;
-        
+
         _timer += Time.deltaTime;
         if (_timer >= _timeToVerifyOutline)
         {
             _isOutlined = false;
-            for (int i = 0; i < _meshRenderersToOutline.Count; i++)
-            {
-                _meshRenderersToOutline[i].materials[_meshRenderersToOutline[i].materials.Length - 1].SetFloat("_outlineAlpha", 0);
-            }
-        } 
+            EnableFeedback(false);
+            // for (int i = 0; i < _meshRenderersToOutline.Count; i++)
+            // {
+            //     _interactText.transform.DOScale(Vector3.zero, .2f).SetEase(Ease.InOutBounce);
+            //     _meshRenderersToOutline[i].materials[_meshRenderersToOutline[i].materials.Length - 1].SetFloat("_outlineAlpha", 0);
+            // }
+        }
+    }
+
+    private void EnableFeedback(bool value)
+    {
+        if (_interactText)
+            _interactText.transform.DOScale(value ? Vector3.one : Vector3.zero, .2f).SetEase(Ease.Linear);
+        for (int i = 0; i < _meshRenderersToOutline.Count; i++)
+            _meshRenderersToOutline[i].materials[_meshRenderersToOutline[i].materials.Length - 1].SetFloat("_outlineAlpha", value ? 1 : 0);
     }
 }
