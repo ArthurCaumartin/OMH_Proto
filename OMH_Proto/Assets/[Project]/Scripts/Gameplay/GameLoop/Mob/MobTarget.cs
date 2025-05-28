ef422b9562ae62aa9ff810bce35b346d15fa8e37
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-05-28 16:06:42 +0200

    respawn sequence insta kill fix

--- OMH_Proto/Assets/[Project]/Scripts/Gameplay/GameLoop/Mob/MobTarget.cs
+++ OMH_Proto/Assets/[Project]/Scripts/Gameplay/GameLoop/Mob/MobTarget.cs
@@ -5,10 +5,10 @@ public class MobTarget : MonoBehaviour
 {
     [SerializeField] private FloatReference _priotity;
     public float Priority { get => _priotity.Value; set => _priotity.Value = value; }
-    public bool enable = true;
+    public bool isActive = true;
 
     public void Enable()
     {
-        enable = true;
+        isActive = true;
     }
 }
