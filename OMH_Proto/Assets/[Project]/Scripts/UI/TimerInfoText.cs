312356337c458fec63a792450b6b036a80cfc723
Duretete Martin <115795428+Dragomal@users.noreply.github.com>
2025-06-05 10:50:47 +0200

    Add may fixs and working tuto infos IG

--- OMH_Proto/Assets/[Project]/Scripts/UI/TimerInfoText.cs
+++ OMH_Proto/Assets/[Project]/Scripts/UI/TimerInfoText.cs
@@ -8,12 +8,21 @@ public class TimerInfoText : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _timerText;
     [SerializeField] private List<GameObject> _timerInfoTexts = new List<GameObject>();
+    [SerializeField] private GameEvent _showStartInfo;
 
     private float _explorationTimer = 0;
     private bool _isDefenseStarted = false;
 
+    private float _timer = 1, _timerTick = 1;
+
     private void Update()
     {
+        _timer += Time.deltaTime;
+        _explorationTimer += Time.deltaTime;
+
+        if (_timer < _timerTick) return;
+        _timer = 0;
+        
         if (_isDefenseStarted)
         {
             _timerInfoTexts[7].SetActive(true);
@@ -22,8 +31,6 @@ public class TimerInfoText : MonoBehaviour
 
         DeactivateAllText();
         
-        _explorationTimer += Time.deltaTime;
-        
         //Explore the base
         if(_explorationTimer < 240) _timerInfoTexts[0].SetActive(true);
         
@@ -31,7 +38,11 @@ public class TimerInfoText : MonoBehaviour
         else if(_explorationTimer < 300) _timerInfoTexts[1].SetActive(true);
         
         //Can start, wave 1
-        else if(_explorationTimer < 360) _timerInfoTexts[2].SetActive(true);
+        else if (_explorationTimer < 360)
+        {
+            _showStartInfo.Raise();
+            _timerInfoTexts[2].SetActive(true);
+        }
         
         //Can start, wave 2
         else if(_explorationTimer < 420) _timerInfoTexts[3].SetActive(true);
